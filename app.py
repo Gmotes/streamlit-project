c6b676fa234468bacbdc414c8e8284b856ab62f5
Burak Tunali <Burak.Tunali+rituals@rituals.com>
2026-05-17 22:14:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -264,7 +264,11 @@ def show_AI_analyst_page():
         input_df.loc[(input_df["Age"] > 75), "Age"] = 75
         input_df["Balance_to_Salary"] = input_df["Balance"] / input_df["EstimatedSalary"]
         input_df["Age_Group"] = pd.cut(input_df["Age"], [0, 30, 45, 65, 99], labels=["18-30", "31-45", "46-65", "66-99"])
-        
+        input_df["NumOfProducts"] = input_df["NumOfProducts"].replace({3: "3 or More", 4: "3 or More"})
+        input_df.loc[(input_df["Age"] > 75), "Age"] = 75
+        input_df["Age_Group"] = pd.cut(input_df["Age"], [0, 30, 45, 65, 99], labels=["18-30", "31-45", "46-65", "66-99"])
+        input_df["Balance_to_Salary"] = input_df["Balance"] / input_df["EstimatedSalary"]
+
         churn_prob = clf.predict_proba(input_df)[0][1]
         churn_label = "High Risk" if churn_prob >= 0.5 else "Low Risk"
 
